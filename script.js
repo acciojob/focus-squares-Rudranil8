eb07991acbd460caf011a26d84f2051fe25e0e2c
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-20 22:27:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let squares= document.querySelectorAll(".square");
+
+squares.forEach(function(square) {
+	square.addEventListener('mouseover', function(){
+		square.style.backgroundColor= "#6F4E37";
+	});
+	square.addEventListener('mouseout', function() {
+		square.style.backgroundColor= "#E6E6FA";
+	});
+}); 
