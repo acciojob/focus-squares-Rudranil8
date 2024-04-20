d266a7d0313dbbf79b02c00b03a9c2605eff3892
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-20 22:30:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 //your JS code here. If required.
-let squares= document.querySelectorAll(".square");
+// let squares= document.querySelectorAll(".square");
+
+// squares.forEach(function(square) {
+// 	square.addEventListener('mouseover', function(){
+// 		square.style.backgroundColor= "#6F4E37";
+// 	});
+// 	square.addEventListener('mouseout', function() {
+// 		square.style.backgroundColor= "#E6E6FA";
+// 	});
+// });
+
+let squares = document.querySelectorAll(".square");
 
 squares.forEach(function(square) {
-	square.addEventListener('mouseover', function(){
-		square.style.backgroundColor= "#6F4E37";
-	});
-	square.addEventListener('mouseout', function() {
-		square.style.backgroundColor= "#E6E6FA";
-	});
-}); 
+    square.addEventListener('mouseover', function(){
+        let otherSquares = document.querySelectorAll(`.square:not(#${square.id})`);
+        otherSquares.forEach(function(otherSquare) {
+            otherSquare.style.backgroundColor = "#6F4E37"; // Coffee color
+        });
+    });
+    square.addEventListener('mouseout', function() {
+        let allSquares = document.querySelectorAll(".square");
+        allSquares.forEach(function(allSquare) {
+            allSquare.style.backgroundColor = "#E6E6FA"; // Lavender color
+        });
+    });
+});
